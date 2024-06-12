1ad869815d06897a76ef61a78b4a65c59049cde5
LostOrphan <30704675+LostOrphan@users.noreply.github.com>
2024-06-12 18:03:04 +0200

    chyba dziala to pi

--- lab11/Rozwpi.java
+++ lab11/Rozwpi.java
@@ -1,5 +1,6 @@
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.math.RoundingMode;
 // import java.math.RoundingMode;
 import java.util.Scanner;
 import java.lang.Math;
@@ -9,28 +10,16 @@ public class Rozwpi {
         System.out.println("Podaj dlugosc rozwinieca: ");
         Scanner in = new Scanner(System.in);
         int iterator=in.nextInt();
-        in.close();
 
-        MathContext mc = new MathContext(20);
         BigDecimal liczba=new BigDecimal("6");
-        double liczbaAdd=0;
+        BigDecimal dodatek=new BigDecimal("0");
+        BigDecimal six=new BigDecimal("6");
         for(int i=1;i<=iterator;i++){
-        liczbaAdd+=6/Math.pow(iterator+1,2);
-        System.out.println(liczbaAdd);
+          dodatek=dodatek.add(six);
+          BigDecimal dzielnik=new BigDecimal((i+1)*(i+1));
+          dodatek=dodatek.divide(dzielnik, new MathContext(iterator,RoundingMode.FLOOR));
+          liczba=liczba.add(dodatek);
         }
-        BigDecimal wynik=new BigDecimal(liczbaAdd);
-        wynik=wynik.add(liczba);
-        
-    //     for(int i=0;i<iterator;i++){
-    //         // BigDecimal dzielnik=new BigDecimal(Math.pow(i + 1, 2));
-    //         // liczba=liczba.add(BigDecimal.valueOf(6)).divide(dzielnik,10,RoundingMode.HALF_UP);
-    //         // BigDecimal dzielnik = new BigDecimal(Math.pow(i + 1, 2), mc);
-    //         // liczba=liczba.add(BigDecimal.valueOf(6));
-    //         // liczba=liczba.divide(dzielnik,mc);
-    //         BigDecimal liczbaDod=new BigDecimal(6);
-    //         liczba=liczba.divide(i+1,mc);
-    //   }
-    //   wynik=wynik.sqrt(mc);
-    //   System.out.println("Liczba pi przyblizona do "+iterator+" liczb wynosi: "+wynik.toString()); 
-}
+        System.out.println("Liczba pi rozwinieta do "+iterator+" miejsc wynosi: "+liczba.sqrt(new MathContext(20,RoundingMode.FLOOR)));        
+      }
 }
